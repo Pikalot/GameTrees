250c62046a195af3da5e08b7aeff6cf3305dd33b
Pikalot <pikalot@hotmail.com>
2025-01-19 03:49:19 -0800

    Check if homepage refresh after updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,20 @@ import TrendingGames from "./ui/components/games/TrendingSection";
 import HighlightStores from "./ui/components/structural/Hero";
 import BestGameDeals from "./ui/components/games/BestGameDeals";
 import { getTrendingGames } from "@/database/queries/game/getTrendingGames";
+import { SeparateStoresRep } from "@/database/queries/store/SeparateStores";
 
 export default async function Home() {
+  const storeRep = new SeparateStoresRep();
+  const highlightStores = await storeRep.getBayAreaStores();
+
   const trendingGames = await getTrendingGames();
+
 
   return (
     <div className="min-h-screen text-gray-100 space-y-12">
       <div>
         {/* Highlight Stores Section  */}
-        <HighlightStores />
+        <HighlightStores stores={highlightStores}/>
       </div>
 
       <hr className="border-2 border-neutral opacity-30 rounded-xl w-full" />
--- documents/database/getStores.sql
+++ documents/database/getStores.sql
@@ -6,7 +6,7 @@
 --     S.city,
 --     SH.day,
 --     SH.start_time AS startTime,
---     SH.end_time AS endTime
+--     SH.end_time AS end_time
 -- FROM Stores S
 -- LEFT JOIN StoreHours SH ON S.sid = SH.sid
 -- WHERE S.city ${isBayArea ? "IN" : "NOT IN"} (${cities.map(() => '?').join(', ')})
