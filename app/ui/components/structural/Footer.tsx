3f73e378b32cd068c285aed251672e54ecbb4e23
Pikalot <pikalot@hotmail.com>
2025-01-19 04:05:28 -0800

    Fixed Footer: change repository link

--- app/ui/components/structural/Footer.tsx
+++ app/ui/components/structural/Footer.tsx
@@ -27,7 +27,7 @@ const Footer = async () => {
             </nav>
             <nav>
                 <div className="grid grid-flow-col gap-4 items-center">
-                    <Link href="https://github.com/KingSand08/GameTrees">
+                    <Link href="https://github.com/Pikalot/GameTrees">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="28"
